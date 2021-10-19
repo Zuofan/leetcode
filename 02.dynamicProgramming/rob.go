c7644e52f5c758224a0ad67138c8dba35d2b9ce1
jzy <wxgoudongxi@163.com>
2021-10-19 21:33:56 +0800

    add rob

---
+++ 02.dynamicProgramming/rob.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(rob([]int{1,2,3,1}))
+}
+
+func rob(costs []int) int {
+	if len(costs) == 1 {
+		return costs[0]
+	}
+
+	p, q, r := costs[0], costs[1], 0
+	if p > q {
+		r = p
+	} else {
+		r = q
+	}
+
+	if len(costs) == 2 {
+		return r
+	}
+
+	q = r
+
+	for i := 2; i < len(costs); i++ {
+
+		r = p + costs[i]
+		if r < q {
+			r = q
+		}
+
+		p = q
+		q = r
+
+	}
+
+	return r
+}
+
