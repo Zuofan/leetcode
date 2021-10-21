004c95bc22094705a6723994039312f1ca8cec9a
jzy <wxgoudongxi@163.com>
2021-10-21 21:56:58 +0800

    modify brute method for sum III

--- 01.twoSum/subarraySum.go
+++ 01.twoSum/subarraySum.go
@@ -1,44 +1,144 @@
 package main
-//Leetcode ：Subarray Sum Equals K
+
+/*
+560. Subarray Sum Equals K
+
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+
+
+*/
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	//violence
-	assertEqual(2, subarraySumViolence([]int{1,2,3}, 3))
-	assertEqual(4, subarraySumViolence([]int{1,2,1,2,1}, 3))
-	assertEqual(2, subarraySumViolence([]int{1,1,1}, 2))
-	assertEqual(1, subarraySumViolence([]int{28,54,7,-70,22,65,-6}, 100))
+	//efficient
+	//assertEqual(2, subarraySum([]int{1,2,3}, 3))
+	//assertEqual(4, subarraySum([]int{1,2,1,2,1}, 3))
+	assertEqual(2, subarraySum([]int{1,1,1}, 2))
+	//assertEqual(1, subarraySum([]int{28,54,7,-70,22,65,-6}, 100))
+	//assertEqual(0, subarraySum([]int{1}, 0))
+	//assertEqual(1, subarraySum([]int{-1,-1,1}, 0))
+	//assertEqual(3, subarraySum([]int{1,-1,0}, 0))
+
+	////brute force
+	//assertEqual(2, subarraySumBruteForce([]int{1,2,3}, 3))
+	//assertEqual(4, subarraySumBruteForce([]int{1,2,1,2,1}, 3))
+	//assertEqual(2, subarraySumBruteForce([]int{1,1,1}, 2))
+	//assertEqual(1, subarraySumBruteForce([]int{28,54,7,-70,22,65,-6}, 100))
 
 }
 
-func assertEqual(expect, actual  int) bool {
-	if expect == actual  {
-		return true
+func subarraySum(nums []int, k int) int {
+	//ver1:wrong!
+	// result := 0
+	// length := len(nums)
+
+	// for i := 0; i < length; i++ {
+	// 	currentSum := nums[i]
+	// 	if currentSum == k {
+	// 		result++
+	// 	}
+	// 	for j := i+1; j < length; j++ {
+	// 		if currentSum + nums[j] == k  {
+	// 			currentSum += nums[j]
+	// 			result++
+	// 		} else {
+	// 			currentSum += nums[j]
+	// 		}
+	// 	}
+	// }
+
+	// return result
+
+	//ver2:wrong!
+	// result := 0
+	// length := len(nums)
+	// prefixSum := make(map[int]int)
+	// prefixSum[0] = 0
+	// currentSum := 0
+
+	// existSum := make(map[int]int)
+	// existSum[0] = 0
+
+	// for j := 0; j < length; j++ {
+	// 	currentSum += nums[j]
+	// 	prefixSum[j+1] = currentSum
+	// 	existSum[currentSum] = j+1
+
+	// 	//fmt.Printf("prefixSum[%d]=%d\n", j+1, prefixSum[j+1])
+	// 	if i, ok := existSum[prefixSum[j+1] - k]; ok && i < j+1 {
+	// 		result++
+	// 	}
+	// }
+
+	// return result
+
+	//ver3:wrong!
+	//count := 0
+	//length := len(nums)
+	//
+	//existSum := make(map[int]int)
+	//existSum[0] = 0
+	//prefixSum := 0
+	//
+	//for j := 0; j < length; j++ {
+	//	prefixSum += nums[j]
+	//	if _, ok := existSum[prefixSum - k]; ok {
+	//		count += 1
+	//	}
+	//
+	//	existSum[prefixSum] = 0
+	//}
+	//
+	//return count
+
+	//ver4:right todo:why
+	count := 0
+	length := len(nums)
+
+	sums := make(map[int]int)
+	sums[0] = 1//?
+	prefixSum := 0
+
+	for j := 0; j < length; j++ {
+		prefixSum += nums[j]
+		count += sums[prefixSum - k]
+		fmt.Printf("prefixSum=%d sums[%d]=%d, count=%d\n", prefixSum, prefixSum, sums[prefixSum],count)
+		sums[prefixSum] = sums[prefixSum] + 1
 	}
 
-	log.Fatalf("expect %d real:%d\n", expect, actual )
-	os.Exit(1)
-	return false
+	return count
 }
 
-/*func assertEqual(t *testing.T, expect, actual interface{}) {
-	t.Helper()
-	if a != b {
-		t.Errorf("Not Equal. %d %d", a, b)
-	}
-}*/
-
-func subarraySumViolence(nums []int, k int) int {
+func subarraySumBruteForce(nums []int, k int) int {
 	result := 0
-	for i := 0; i < len(nums); i++ {
+	length := len(nums)
+
+	for i := 0; i < length; i++ {
 		currentSum := nums[i]
 		if currentSum == k {
 			result++
 		}
-		for j := i+1; j < len(nums); j++ {
+		for j := i+1; j < length; j++ {
 			if currentSum + nums[j] == k  {
 				currentSum += nums[j]
 				result++
@@ -49,4 +149,14 @@ func subarraySumViolence(nums []int, k int) int {
 	}
 
 	return result
+}
+
+func assertEqual(expect, actual  int) bool {
+	if expect == actual  {
+		return true
+	}
+
+	log.Fatalf("expect %d real:%d\n", expect, actual )
+	os.Exit(1)
+	return false
 }
--- 01.twoSum/sum3.go
+++ 01.twoSum/sum3.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	//sum3([]int{-1,0,1,2,-1,-4})
-	sum3([]int{0,0,0,0})
+	fmt.Println(threeSum([]int{0,0,0,0}))
 
 	//[-1,0,1,2,-1,-4,-2,-3,3,0,4]
 	//[[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
@@ -28,66 +28,34 @@ func main() {
 }
 
 
-func sum3(elems []int) [][]int {
+func threeSum(elems []int) [][]int {
 	sort.Ints(elems)
+	var results [][]int
 
-	var result [][]int
-
-	cache := make(map[string]int)
-
-	for i := 0; i < len(elems); i++ {
-		if _, ok := cache[k(i, 0, 0)]; ok {
+	for i:= 0; i < len(elems); i++ {
+		if i > 0 && elems[i] == elems[i-1] {
 			continue
 		}
-		p, q, r := sum3Helper(elems, i)
-		//fmt.Printf("%d %d %d result=%t\n",elems[i], elems[p], elems[q], r)
-		if r {
-			currentIndex := i
-			if currentIndex > q {
-				currentIndex, p, q = p, q, currentIndex
-			} else if currentIndex > p {
-				currentIndex, p, q = p, currentIndex, q
-			}
 
-			if _, ok := cache[k(elems[currentIndex], elems[p], elems[q])]; ok {
+		for j:= i+1; j < len(elems); j++ {
+
+			if j > i+1 && elems[j] == elems[j-1] {
 				continue
 			}
-			result = append(result, []int{elems[currentIndex], elems[p], elems[q]})
-			cache[k(elems[currentIndex], elems[p], elems[q])] = currentIndex+p+q
-			fmt.Printf("%d %d %d\n",currentIndex, p, q)
-			cache[k(i, 0, 0)] = i
-			cache[k(p, 0, 0)] = p
-			cache[k(q, 0, 0)] = q
-		}
-	}
-	fmt.Println(cache)
-	return result
-}
 
-func k(i,p,q int) string {
-	return fmt.Sprintf("%d%d%d", i, p, q)
-}
+			for m := j+1; m < len(elems); m++ {
+				if m > j+1 && elems[m] == elems[m-1] {
+					continue
+				}
 
-func sum3Helper(elems []int, index int) (int, int, bool) {
-	target := 0-elems[index]
-	for i, j := 0, len(elems) - 1; i < j; {
-		if i == index {
-			i++
-			continue
-		} else if j == index {
-			j--
-			continue
-		}
-		sum := elems[i] + elems[j]
-		if target > sum {
-			i++
-		} else if target < sum {
-			j--
-		} else {
-			return i, j, true
+				if elems[i] + elems[j] + elems[m] == 0 {
+					results = append(results, []int{elems[i], elems[j], elems[m]})
+					continue
+				}
+
+			}
 		}
 	}
 
-	return 0, 0, false
+	return results
 }
-
