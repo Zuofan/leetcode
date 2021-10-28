663391c4bbc1ddb662f7c35abe1f0a6fca0f1a30
jzy <jzy@yuanxin>
2021-10-28 22:03:38 +0800

    add singleNumber

---
+++ 04.link/singleNumber_test.go
@@ -0,0 +1,41 @@
+package _04_link
+
+import (
+	"sort"
+	"testing"
+)
+
+
+func singleNumber(nums []int) int {
+	sort.Ints(nums)
+	i := 0
+	for ; i + 1 < len(nums);  {
+		if nums[i] == nums[i+1] {
+			i += 2
+		} else {
+			break
+		}
+	}
+
+	return nums[i]
+	//
+	//return r
+}
+
+
+//!+test
+func TestSingleNumber(t *testing.T) {
+	var tests = []struct {
+		input []int
+		want  int
+	}{
+		{[]int{2,2,1}, 1},
+		{[]int{4,1,2,1,2}, 4},
+	}
+	for _, test := range tests {
+		if got := singleNumber(test.input); got != test.want {
+			t.Errorf("singleNumber(%q) = %v", test.input, got)
+		}
+	}
+}
+//!-test
