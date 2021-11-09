f0ee91c48e665959984abc4e844681a018163ad4
jzy <letcode@lean-reading.com>
2021-11-09 10:41:54 +0800

    add Floyd Cycle Detection Algorithm for 141:linked List Cycle Problem

--- 04.link/linkedListCycle141_test.go
+++ 04.link/linkedListCycle141_test.go
@@ -22,28 +22,73 @@ func addHelper(t *CycleListNode, value int) *CycleListNode {
 	return t
 }
 
+
 func add(elems []int, cyclePos int) *CycleListNode {
-	var r *CycleListNode
-	var cyclePoint *CycleListNode
-	var tail *CycleListNode
+	links := make([]*CycleListNode, len(elems))
 
 	for i, elem := range elems {
-		r = addHelper(r, elem)
-		if i == cyclePos {
-			cyclePoint = r
+		t := new(CycleListNode) // equal to &tree{value: value}
+		t.Val = elem
+
+		links[i] = t
+	}
+
+
+	for i, _ := range elems {
+		if i < len(elems) - 1 {
+			links[i].Next = links[i+1]
+		} else if cyclePos >= 0 {
+			links[len(elems) - 1].Next = links[cyclePos]
 		}
 
-		if len(elems) - 1 == i {
-			tail = r
+	}
+
+	return links[0]
+}
+
+
+//Floyd Cycle Detection Algorithm
+func hasCycle(head *CycleListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	pslow := head
+	pfast := head.Next.Next
+	for ; pslow != pfast; {
+		if pslow.Next == nil || pfast == nil {
+			break
+		}
+
+		pslow = pslow.Next
+
+		if pfast != nil && pfast.Next != nil && pfast.Next.Next != nil {
+			pfast = pfast.Next.Next
 		}
 	}
 
-	tail.Next = cyclePoint
-
-	return r
+	return pslow == pfast
 }
 
-func hasCycle(head *CycleListNode) bool {
+func TestHasCycle(t *testing.T) {
+	var tests = []struct {
+		input *CycleListNode
+		want  bool
+	}{
+		{add([]int{3,2,0,-4}, 1), true},
+		{add([]int{1,2}, 0), true},
+		{add([]int{1}, -1), false},
+		{add([]int{1, 2}, -1), false},
+	}
+	for j, test := range tests {
+		if got := hasCycle(test.input); got != test.want {
+			t.Errorf("%d:hasCycle(%v) = %v", j, test.input, got)
+		}
+	}
+}
+
+//hash resolution
+func hasCycleV1(head *CycleListNode) bool {
 	cache := make(map[*CycleListNode]bool)
 
 	for; head != nil; head = head.Next {
@@ -57,7 +102,7 @@ func hasCycle(head *CycleListNode) bool {
 
 
 //!+test
-func TestHasCycle(t *testing.T) {
+func TestHasCycleV1(t *testing.T) {
 	var tests = []struct {
 		input *CycleListNode
 		want  bool
