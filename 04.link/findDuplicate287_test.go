364ee2996ce26dc6713d0dbab7d4a9d9e1efe10d
jzy <letcode@lean-reading.com>
2021-11-16 19:43:33 +0800

    add new resolution

--- 04.link/findDuplicate287_test.go
+++ 04.link/findDuplicate287_test.go
@@ -4,8 +4,8 @@ package _04_link
 
 import "testing"
 
-
-func findDuplicate(nums []int) int {
+// cache resolution
+func findDuplicate_cacheResolution(nums []int) int {
 	cache := make(map[int]int)
 
 	for _, elem := range nums {
@@ -21,6 +21,53 @@ func findDuplicate(nums []int) int {
 	return 0
 }
 
+
+// binary search resolution
+func findDuplicate_binarySearchResolution(nums []int) int {
+	len := len(nums)
+	left, right := 1, len-1
+
+	for ; left != right; {
+		mid := left + (right - left) / 2
+		cnt := 0
+		for _, elem := range nums {
+			if elem <= mid {
+				cnt++
+			}
+		}
+
+		if cnt > mid {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
+func findDuplicate(nums []int) int {
+	r := 0
+
+	for i := 0; i < 32; i++ {
+		normal  := 0
+		unusual := 0
+		for j, elem := range nums {
+			if j > 0 {
+				normal += (j >> i) & 0x1
+			}
+
+			unusual += (elem >> i) & 0x1
+		}
+
+		if unusual > normal {
+			r += 1 << i
+		}
+
+	}
+
+	return r
+}
 
 //!+test
 func TestFindDuplicate(t *testing.T) {
